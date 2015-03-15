8f432127424b2e1b899b9e21e416a3ab9da3f68c
Lukas Prediger <lukas.prediger@rwth-aachen.de>
2015-03-15 16:51:27 +0100

    Created script InitializePulseData which initializes the global plsdata struct.

--- Testing/hardwarePulseTest.m
+++ Testing/hardwarePulseTest.m
@@ -1,15 +1,5 @@
 function hardwarePulseTest()
-    global plsdata;
-    plsdata = [];
-    plsdata.datafile = [tempdir 'hardwaretest\plsdata_hw'];
-    plsdata.grpdir = [tempdir 'hardwaretest\plsdef\plsgrp'];
-    try
-        rmdir([tempdir 'hardwaretest'],'s');
-    end
-    mkdir(plsdata.grpdir);
-    
-    plsdata.pulses = struct('data', {}, 'name', {},	'xval',{}, 'taurc',{}, 'pardef',{},'trafofn',{},'format',{});
-    plsdata.tbase = 1000;
+    InitializePulseData;
 
 
     testSetup = RawIOTestSetup();
