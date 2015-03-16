316a52a223c405bdf2e0dc9fc5590c5f530475a3
Patrick Bethke <bethke@physik.rwth-aachen.de>
2015-03-16 11:29:16 +0100

    reformat DSIOTestSetup pulse initialization for readability

--- Testing/DSIOTestSetup.m
+++ Testing/DSIOTestSetup.m
@@ -48,12 +48,8 @@ classdef DSIOTestSetup < DefaultTestSetup
             for i = 1:self.test_iterations
                 randomValue = randomValues(i);
                 pulse.data.pulsetab = zeros(2, 6);
-                pulse.data.pulsetab(1,:) = [0, self.test_start - dt, ...
-                    self.test_start, self.test_end, self.test_end + dt, ...
-                    self.test_period];
-                pulse.data.pulsetab(2,:) = [-randomValue, -randomValue, ...
-                    randomValue, randomValue, -randomValue, ...
-                    -randomValue];
+                pulse.data.pulsetab(1,:) = [0,            self.test_start - dt,    self.test_start, self.test_end, self.test_end + dt, self.test_period];
+                pulse.data.pulsetab(2,:) = [-randomValue, -randomValue,            randomValue,     randomValue,   -randomValue,       -randomValue];
                 pulse.name = sprintf('DSIOTestPulse%i', i);
                 self.pulsegroup.pulses(i) = plsreg(pulse);
             end            
